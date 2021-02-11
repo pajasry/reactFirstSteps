9e880b1131bd7a2111f8a61d33921b6e90fc1a7f
Pavel Šrytr <pajasry@gmail.com>
2021-02-11 21:29:21 +0000

    presentation

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -5,18 +5,14 @@ import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
 class App extends Component {
     render() {
         return (
-            <Paper
-                elevation={0}
-                style={{ padding: 0, margin: 0, backgroundColor: '#fafafa' }}
-            >
+            <>
                 <AppBar color="primary" position="static" style={{ height: 64 }}>
                     <Toolbar style={{ height: 64 }}>
                         <Typography color="inherit">TodoList</Typography>
                     </Toolbar>
                 </AppBar>
                 <TodoList/>
-                <TodoList/>
-            </Paper>
+            </>
         )
     }
 }
